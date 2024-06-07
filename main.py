8b22a130788e624deb115a0edb3387c0551f31e7
Ing. Gabriel Gonzalez <74801368+Gibbigon69@users.noreply.github.com>
2024-06-06 19:19:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ app.register_blueprint(prove)
 app.register_blueprint(marcas)
 CORS(app)
 
+@app.route('/')
+def index():
+    return 'Bienvenido a la api'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
